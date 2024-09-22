cd12b2c43a0fbb97a493de97a128084ba8cbd84f
beingyash <beingyash2712@gmail.com>
2024-09-23 00:25:47 +0530

    theme working in web but not in android

--- src/app/(tabs)/index.js
+++ src/app/(tabs)/index.js
@@ -1,4 +1,4 @@
-import { View, Image , StyleSheet } from "react-native";
+import { View, Image , StyleSheet  , Dimensions} from "react-native";
 import {
   Avatar,
   Button,
@@ -9,14 +9,23 @@ import {
   Appbar,
 } from "react-native-paper";
 import React, { useState } from "react";
+import { useTheme } from "../../hooks/useTheme";
 
-const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+// const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+const {width, height} = Dimensions.get('window');
 
 export default function Index() {
   const [search, setSearch] = useState("");
 
+  const { colorScheme } = useTheme(); // Get the current theme
+  console.log("Current Theme:", colorScheme); // Log the current theme
+  const welcomeTextColor = colorScheme === 'dark' ? '#F6E5E2' : '#52433F';
+  const buttonTextColor = colorScheme === 'dark' ? '#F6E5E2' : '#52433F';
+  const buttonColor = colorScheme === 'dark' ? '#392B27' : '#F6E5E2';
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+
       <View style={styles.iconButton}>
         <IconButton
           icon="menu"
@@ -40,21 +49,105 @@ export default function Index() {
         />
       </View>
     </View>
+    <View style={styles.welcome}>
+    <Text style={[styles.welcomeText, { color: welcomeTextColor }]}>
+          Welcome to FLEXBOX
+        </Text>
+    </View>
+    <View style={styles.company}>
+      <Image style={styles.companyimage}
+        source={{
+          uri: "https://media.licdn.com/dms/image/v2/D5605AQF8rNEvYF7L5g/feedshare-thumbnail_720_1280/feedshare-thumbnail_720_1280/0/1686665424139?e=2147483647&v=beta&t=9CEO0cJxmGoeLTh6y4OvshuiXDmxeTmQTzU1CxXCO1g",
+        }}   
+      />
+    </View>
+    <View style={styles.buttons}>
+      <Button style={[styles.button1, { backgroundColor: buttonColor }]}>
+        <Text style={[styles.buttonText, { color: buttonTextColor }]}>Capital</Text>
+      </Button>
+      <Button style={[styles.button1, { backgroundColor: buttonColor }]}>
+        <Text style={[styles.buttonText, { color: buttonTextColor }]}>Campus</Text>
+      </Button>
+    </View>
+    </View>    
   );
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flex:1,
+  },
+  header: {
+    flex: 1/9,
+    justifyContent: "space-between",
+    alignItems:"center",
     flexDirection: "row",
+  },
+  searchBar: {
+    // flex:1/2,
+    width:'50%'
   },
   image: {
     marginLeft: 10,
+    marginRight:10
   },
   iconButton: {
     marginLeft: 10,
   }, 
+  welcome: {
+    flex: 1/18,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop:(1/18)*height,
+    marginBottom:(2/18)*height,
+  },
+  welcomeText: {
+    fontSize: 24, // Set font size
+    fontWeight: '900', // Make the text bold
+    color: '#52433F', // Set text color
+    textAlign: 'center', // Center the text
+    padding: 10, // Add padding around the text
+  },
+  company: {
+    flex: 7/18,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  companyimage:{
+    height: '100%',
+    width: '50%',
+  },
+  button1: {
+    width: '50%',
+    padding: 10, // Add padding for better touch area
+    backgroundColor: '#F6E5E2', // Button color
+    borderRadius: 5, // Rounded corners
+    marginRight: 5, // Space between buttons
+  },
+  button2: {
+    width: '50%',
+    padding: 10, // Add padding for better touch area
+   backgroundColor: '#F6E5E2', // Button colorn color
+    borderRadius: 5, // Rounded corners
+    marginLeft: 5, // Space between buttons
+  },
+  buttons: {
+    flex: 2/18,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop : (1/18)*height,
+  }
+  ,
+  buttonText: {
+    fontSize: 18, // Set font size
+    fontWeight: '900', // Make the text bold
+    color: '#52433F', // Set text color
+    textAlign: 'center', // Center the text
+    padding: 10, // Add padding around the text
+  }
+
+
+  
 });
